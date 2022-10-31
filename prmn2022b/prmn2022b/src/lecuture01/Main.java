d86e39680d674ca0848004a82fb2d79247bc4040
b2210180 <b2210180@photon.chitose.ac.jp>
2022-10-31 20:40:17 +0900

    課題１

---
+++ prmn2022b/prmn2022b/src/lecuture01/Main.java
@@ -0,0 +1,12 @@
+package lecuture01;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("b2210180 Yu Ichihashi");
+    }
+}
+
+
