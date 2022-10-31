0300ee499f6cb3e3304e35c69539e33cbda85c09
b2210180 <b2210180@photon.chitose.ac.jp>
2022-10-31 21:02:42 +0900

    課題４

--- prmn2022b/prmn2022b/src/lecuture01/Main.java
+++ prmn2022b/prmn2022b/src/lecuture01/Main.java
@@ -10,18 +10,29 @@ public class Main {
         //int studentNumber = 2210180;
         //System.out.println("b" + studentNumber + " Yu Ichihashi");
 
-        Scanner input = new Scanner(System.in);
-          int age = input.nextInt();
+        //if(age < 20 && 0 >= age) {
+        //  System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        //}
+        //else if(age < 0){
+        //  System.out.println("Error");
+        //}
+        //else{
+        //  System.out.println("I am " + age +" years old so I can drink liquor.");
+        //}
 
-        if(age < 20 && 0 >= age) {
-          System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        int[] array = new int[100];
+
+        int x = 1;
+        int sum = 0;
+
+        for(int i = 0;i < array.length;i++){
+        array[i] = x;
+        if(x % 2 == 0){
+        sum = sum + x;
         }
-        else if(age < 0){
-          System.out.println("Error");
+        x = x + 1;
         }
-        else{
-          System.out.println("I am " + age +" years old so I can drink liquor.");
-        }
+        System.out.println(sum);
     }
 }
 
