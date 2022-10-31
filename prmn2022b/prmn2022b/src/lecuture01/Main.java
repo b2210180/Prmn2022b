438609ee6f2fd0ef9448d85276af7cc718caebd9
b2210180 <b2210180@photon.chitose.ac.jp>
2022-10-31 20:42:37 +0900

    課題２

--- prmn2022b/prmn2022b/src/lecuture01/Main.java
+++ prmn2022b/prmn2022b/src/lecuture01/Main.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("b2210180 Yu Ichihashi");
+        //System.out.println("b2210180 Yu Ichihashi");
+
+        int studentNumber = 2210180;
+        System.out.println("b" + studentNumber + " Yu Ichihashi");
     }
 }
 
