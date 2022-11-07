9c62cd1d55b153680fdf5fa555b88e9a9f451075
b2210180 <b2210180@photon.chitose.ac.jp>
2022-11-07 15:57:05 +0900

    課題3

--- prmn2022b/prmn2022b/src/lecture02/Engine.java
+++ prmn2022b/prmn2022b/src/lecture02/Engine.java
@@ -1,9 +1,12 @@
 package lecture02;
 
+
+
 public class Engine {
-    int rpm = 4000;
+    int rpm;
 
     void  startEngine(){
+        int rpm = 4000;
         System.out.println("rpm = " + rpm + "でエンジンを始動させました。");
     }
 }
